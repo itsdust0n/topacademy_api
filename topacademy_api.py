d9cded1abab3f2d1131ce601b177ae7167c96d2e
Egor A. Balenko <dust0n@mail.ru>
2024-05-27 16:55:48 +0500

    > topacademy_api.py: refactoring and try/except added

--- topacademy_api.py
+++ topacademy_api.py
@@ -1,34 +1,36 @@
 import requests
+import logging
 
 
 class JournalApi:
     def __init__(self, authorization):
-        self.BASE_URL = 'https://msapi.top-academy.ru/api/v2'
         self.session = requests.Session()
         self.session.headers.update({
             "authorization": f"{authorization}",
             "referer": "https://journal.top-academy.ru/"
         })
 
+    def _get(self, endpoint, method):
+        url = f"https://msapi.top-academy.ru/api/v2/{endpoint}"
+        try:
+            r = response = self.session.request(method, url=url)
+            r.raise_for_status()
+            return r.json()
+        except requests.RequestException as e:
+            logging.error(f'Request to {url} failed: {e}')
+
+
     def get_future_exams(self):
-        r = self.session.get(f"{self.BASE_URL}/dashboard/info/future-exams")
-        r.raise_for_status()
-        return r.json()
+        return self._get("dashboard/info/future-exams", "GET")
 
     def get_attendance(self):
-        r = self.session.get(f"{self.BASE_URL}/dashboard/chart/attendance")
-        r.raise_for_status()
-        return r.json()
+        return self._get("dashboard/chart/attendance", "GET")
 
     def get_schedule_month(self, date):
-        r = self.session.get(f"{self.BASE_URL}/schedule/operations/get-month?date_filter={date}")
-        r.raise_for_status()
-        return r.json()
+        return self._get(f"schedule/operations/get-month?date_filter={date}")
 
     def get_schedule_day(self, date):
-        r = self.session.get(f"{self.BASE_URL}/schedule/operations/get-by-date?date_filter={date}")
-        r.raise_for_status()
-        return r.json()
+        return self._get(f"schedule/operations/get-by-date?date_filter={date}")
 
 
 class OmniApi():
