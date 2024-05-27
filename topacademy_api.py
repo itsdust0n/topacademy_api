69facfb9593c8059e4d39ddbf0a2b6739409d6ce
Egor A. Balenko <dust0n@mail.ru>
2024-05-27 13:23:00 +0500

    > topacademy_api.py: small refactoring

--- topacademy_api.py
+++ topacademy_api.py
@@ -4,10 +4,9 @@ import requests
 class JournalApi():
     def __init__(self, authorization):
         self.headers = {
-            "authorization": "",
+            "authorization": f"{authorization}",
             "referer": "https://journal.top-academy.ru/"
         }
-        self.headers["authorization"] = authorization
 
     def get_future_exams(self):
         url = "https://msapi.top-academy.ru/api/v2/dashboard/info/future-exams"
