c883dafb73b735f86f178f17e4b90e674ad7efd3
Egor A. Balenko <dust0n@mail.ru>
2024-05-27 12:44:10 +0500

    > topacademy_api.py: added ability to get student's upcoming exams

--- topacademy_api.py
+++ topacademy_api.py
@@ -8,3 +8,8 @@ class JournalApi():
             "referer": "https://journal.top-academy.ru/"
         }
         self.headers["authorization"] = authorization
+
+    def get_future_exams(self):
+        url = "https://msapi.top-academy.ru/api/v2/dashboard/info/future-exams"
+        r = requests.get(url, headers=self.headers)
+        return r.json()
