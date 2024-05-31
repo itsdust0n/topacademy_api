ee32856e47519362c6b18ce7c34e56fbd87d9ccc
Egor A. Balenko <contact@dust0n.ru>
2024-05-31 19:17:49 +0500

    > setup.py, setup.cfg: added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='topacademy_api',
+      version='1.0',
+      description='A library for Python that provides an easy interface to interact with the TOP Academy resources',
+      packages=['topacademy_api'],
+      author_email='accounts@dust0n.ru',
+      zip_safe=False)
